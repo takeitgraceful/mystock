33f8cf90237cbb43610e6502fa0782d9300b31e3
takeitgraceful <takeitgraceful@gmail.com>
2019-03-24 19:27:45 -0400

    Unable to run npm

--- app.js
+++ app.js
@@ -1,12 +1,68 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var okta = require("@okta/okta-sdk-nodejs");
+var ExpressOIDC = require("@okta/oidc-middleware").ExpressOIDC;
+
+app.use(session({
+  secret: 'asdf;lkjh3lkjh235l23h5l235kjh',
+  resave: true,
+  saveUninitialized: false
+}));
+app.use(oidc.router);
+
+app.use((req, res, next) => {
+  if (!req.userinfo) {
+    return next();
+  }
+
+  oktaClient.getUser(req.userinfo.sub)
+    .then(user => {
+      req.user = user;
+      res.locals.user = user;
+      next();
+    }).catch(err => {
+      next(err);
+    });
+});
+
+function loginRequired(req, res, next) {
+  if (!req.user) {
+    return res.status(401).render("unauthenticated");
+  }
+
+  next();
+}
 
 const dashboardRouter = require("./routes/dashboard");
 const publicRouter = require("./routes/public");
+const usersRouter = require("./routes/users");
+
 
 var app = express();
+var oktaClient = new okta.Client({
+  orgUrl: 'dev-428778.okta.com',
+  token: '000i-7WBb0eWkYYOQYzmrHtm1iUm9wZVDWKbJb6lXn'
+});
+const oidc = new ExpressOIDC({
+  issuer: "dev-428778.okta.com/oauth2/default",
+  client_id: "0oacgt1iiTRkrkhVB356",
+  client_secret: "FqxxRV_DhkHe4uYD1qD_sXOkTjcQGkXlMHfWhxGg",
+  redirect_uri: 'http://localhost:3000/users/callback',
+  scope: "openid profile",
+  routes: {
+    login: {
+      path: "/users/login"
+    },
+    callback: {
+      path: "/users/callback",
+      defaultRedirect: "/dashboard"
+    }
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,7 +74,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', publicRouter);
-app.use('/dashboard', dashboardRouter);
+app.use('/dashboard', loginRequired, dashboardRouter);
+app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
