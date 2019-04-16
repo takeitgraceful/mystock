de510605b0af94932dc80010905e9ec167eb7bdc
takeitgraceful <takeitgraceful@gmail.com>
2019-04-16 18:08:01 -0400

    Updates to Guide

--- app.js
+++ app.js
@@ -1,84 +1,37 @@
-var http = require('http'),
-    path = require('path'),
-    methods = require('methods'),
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    session = require('express-session'),
-    cors = require('cors'),
-    passport = require('passport'),
-    errorhandler = require('errorhandler'),
-    mongoose = require('mongoose');
+var express = require('express');
+require('dotenv').config();
+const PORT = process.env.PORT || 5000
 
-var isProduction = process.env.NODE_ENV === 'production';
+var flash = require('connect-flash');
 
-// Create global app object
+var passport = require("passport");
+var request = require('request');
+
+var session = require("express-session");
+
 var app = express();
 
-app.use(cors());
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
 
-// Normal express config defaults
-app.use(require('morgan')('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-app.use(require('method-override')());
-app.use(express.static(__dirname + '/public'));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
+var bodyParser = require('body-parser')
 
-if (!isProduction) {
-  app.use(errorhandler());
-}
+var path = require('path');
 
-if(isProduction){
-  mongoose.connect(process.env.MONGODB_URI);
-} else {
-  mongoose.connect('mongodb://localhost/conduit');
-  mongoose.set('debug', true);
-}
+app.use('/public', express.static(__dirname + '/public'));
 
-require('./models/User');
-require('./models/Article');
-require('./models/Comment');
-require('./config/passport');
+app.use(flash());
+app.use(session({secret: 'keyboard cat'}))
+app.use(bodyParser());
+app.set('view engine', 'pug');
+app.set('view options', { layout: false });
 
-app.use(require('./routes'));
 
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+require('./lib/routes.js')(app);
 
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (!isProduction) {
-  app.use(function(err, req, res, next) {
-    console.log(err.stack);
-
-    res.status(err.status || 500);
-
-    res.json({'errors': {
-      message: err.message,
-      error: err
-    }});
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({'errors': {
-    message: err.message,
-    error: {}
-  }});
-});
-
-// finally, let's start our server...
-var server = app.listen( process.env.PORT || 3000, function(){
-  console.log('Listening on port ' + server.address().port);
-});
+app.listen(PORT);
+console.log('Node listening on port %s', PORT);
