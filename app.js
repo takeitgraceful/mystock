990d158bc7d12bea623aecdd6a739e4b3207d412
takeitgraceful <takeitgraceful@gmail.com>
2020-04-05 01:43:36 -0400

    mystock

--- app.js
+++ app.js
@@ -1,37 +1,41 @@
+var createError = require('http-errors');
 var express = require('express');
-require('dotenv').config();
-const PORT = process.env.PORT || 5000
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-var flash = require('connect-flash');
-
-var passport = require("passport");
-var request = require('request');
-
-var session = require("express-session");
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-var bodyParser = require('body-parser')
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-var path = require('path');
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.use('/public', express.static(__dirname + '/public'));
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-app.use(flash());
-app.use(session({secret: 'keyboard cat'}))
-app.use(bodyParser());
-app.set('view engine', 'pug');
-app.set('view options', { layout: false });
-
-
-require('./lib/routes.js')(app);
-
-app.listen(PORT);
-console.log('Node listening on port %s', PORT);
+module.exports = app;
