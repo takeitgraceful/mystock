46cf2ef36ad49017566e1a96c6c4dd3294432be7
takeitgraceful <takeitgraceful@gmail.com>
2020-05-18 21:36:39 -0400

    Updates to Site

--- config/auth.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  secret: "S5E$&JJPnT(cQuS"
-};
--- database.js
+++
@@ -1,2 +0,0 @@
-var pgp = require("pg-promise")(/*options*/);
-var db = pgp("postgres://${process.env.DATA_DB_USER}:${process.env.DATA_DB_PASS}@${process.env.DATA_DB_HOST}/gonano");
